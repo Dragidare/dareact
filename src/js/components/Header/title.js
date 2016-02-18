ec9d442df90b49f3ee6b078a31eb5eebe6d2be9a
Darko Antic <antic.darko@gmail.com>
2016-02-18 15:49:53 +0100

    Tag 2

---
+++ src/js/components/Header/title.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class Title extends React.Component {
+  render() {
+        return (
+          <div class="container">
+            <div class="intro-text">
+                <div class="intro-lead-in">Welcome To Our Studio!</div>
+                <div class="intro-heading">Its Nice To Meet You</div>
+                <a href="#services" class="page-scroll btn btn-xl">Tell Me More</a>
+            </div>
+        </div>
+    );
+  }
+}
